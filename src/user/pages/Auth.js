2fed2baa4f26dd22ab1ef18f54b4084a4e729d00
Ash0x <alex.galev@yahoo.com>
2021-12-25 18:07:45 -0800

    implemented error handling for login/signup

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -57,11 +57,34 @@ const Auth = () => {
 
   const authSubmitHandler = async (event) => {
     event.preventDefault()
+ 
+    setIsLoading(true)
 
     if (isLoginMode) {
+      try {
+        const response = await fetch('http://localhost:5000/api/users/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        })
+        const responseData = await response.json()
+        if (!response.ok) {
+          throw new Error(responseData)
+        }
+        setIsLoading(false)
+        auth.login()
+      } catch (err) {
+        console.log(err)
+        setIsLoading(false)
+        setError(err.message || 'Something went wrong, please try again.')
+      }
     } else {
       try {
-        setIsLoading(true)
         const response = await fetch('http://localhost:5000/api/users/signup', {
           method: 'POST',
           headers: {
@@ -75,9 +98,8 @@ const Auth = () => {
         })
         const responseData = await response.json()
         if (!response.ok) {
-          throw new Error(responseData.message)
+          throw new Error(responseData)
         }
-        console.log(responseData)
         setIsLoading(false)
         auth.login()
       } catch (err) {
